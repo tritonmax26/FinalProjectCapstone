38270be49cfaec650655a9685ee80da6be4c08a1
myke023 <pmiranda3333@gmail.com>
2023-05-15 20:24:26 +0800

    renderfiles

--- src/lib/http.js
+++ src/lib/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: import.meta.env.VITE_API,
 
     headers:{
         'Accept' : 'application/json',
